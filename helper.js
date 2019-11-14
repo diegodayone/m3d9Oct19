be811f198fa039567c17a4e882b42f571fc2e6d4
Diego Banovaz <diego@strive.school>
2019-11-14 11:37:43 +0100

    last

--- helper.js
+++ helper.js
@@ -10,7 +10,7 @@ getEvent = async id => {
   return await response.json(); //this is transforming the response in a json
 };
 
-saveEvent = async event => {
+saveEvent = async event => { // no ID in the url, but information in the body
   const response = await fetch(url, {
     //the POST is made with the fetch method as well
     method: "POST", //declaring the CRUD method
@@ -22,7 +22,7 @@ saveEvent = async event => {
   return response; //returning the response because the frontend need to check the ok property
 };
 
-updateEvent = async (id, event) => {
+updateEvent = async (id, event) => { //PUT usually have the url followed by the ID of the item we want to change / edit /api/agenda/id
   const response = await fetch(url + id, {
     method: "PUT",
     body: JSON.stringify(event),
@@ -34,8 +34,11 @@ updateEvent = async (id, event) => {
   return response;
 }
 
-deleteEvent = async id => {
+deleteEvent = async id => { //DELETE has no body / payload, just the ID in the url /api/agenda/id
   const response = await fetch(url + id, {
+    // headers: {
+    //   "Authorization": "Basic whatever"
+    // },
     method: "DELETE"
   })
   return response.ok;
